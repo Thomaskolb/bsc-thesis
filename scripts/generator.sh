29ab1271ffdae86d4fd8e993a86299d248910c75
Thomas <thomaskolb98@gmail.com>
2021-10-15 14:54:39 +0200

    Add generator.sh bash script

---
+++ scripts/generator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+#SBATCH --partition=csedu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=1G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:15:00
+#SBATCH --output=filter-%J.out
+#SBATCH --error=filter-%J.err
+#SBATCH --mail-user=thomaskolb@live.nl
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+source "/home/tkolb/bsc/bsc-thesis/env/bin/activate"
+python3 samplegenerator.py "/home/tkolb/bsc/bsc-thesis/filtered_data.txt" "/home/tkolb/bsc/thomas/data" "/home/tkolb/bsc/outputdata" 
+deactivate
