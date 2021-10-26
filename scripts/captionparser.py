3e69e030f9e75191e54100693361b70a2f344514
Thomas <thomaskolb98@gmail.com>
2021-10-26 20:12:13 +0200

    Add new code with configurations

--- scripts/captionparser.py
+++ scripts/captionparser.py
@@ -1,15 +1,33 @@
 # Thomas Kolb s1027332
 # This program contains functions to parse a caption text to match the appropriate rules for training
 
-from num2words import num2words
-from nltk.tokenize import word_tokenize
+# from num2words import num2words
+# from nltk.tokenize import word_tokenize
 import re
 
-import nltk
-nltk.download('punkt')
+# import nltk
+# nltk.download('punkt')
 
 interpunction = ['.', '...', '!', '?']
 forbidden_formats = ['.{1,}\-.{1,}', '[A-Z]{1,}\:', '[0-9]{1,}\.[0-9]{1,}', '[0-9]{1,}e']
+
+
+# Configurations
+# Currently active configuration
+active_config = 1
+# Allow for capitalization of first word of sentence in configuration
+config_capitalization = [False, True, True]
+# Allowed characters
+config_allowed = []
+# Ignore these characters completely by removing them
+config_ignore = []
+# Convert characters of dict from key to value
+config_convert = [] 
+
+# Configuration 1
+config_allowed.append(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', '\''])
+config_ignore.append(['.', ',', '!', '?', '-', ':', '%', '&', '`'])
+config_convert.append({'é':'e', 'è':'e', 'ë':'e', 'ê':'e', 'ö':'o', 'ó':'o', 'ï':'i', 'ü':'u', })
 
 # Function that filters out captions that don't match requirements
 # If it is acceptable it will return an edited caption text
@@ -19,19 +37,39 @@ def acceptable_caption_text(caption_text):
     follow_with_capital = False
     for word in word_list:
         # Filter for all upper letters, star symbol and regex formats
-        if (word.isupper() and len(word) > 1) or word == '*' or any([re.match(exp, word) for exp in forbidden_formats]):
+        if ((word.isupper() and len(word) > 1)
+            or word == '*' 
+            or any([re.match(exp, word) for exp in forbidden_formats])):
             return ''
         word = number_to_words(word)
         # Capitalize word after interpunction (except ',')
         if follow_with_capital:
             word = word.capitalize()
             follow_with_capital = False
-        new_word_list.append(word)
-        if word in interpunction:
+        new_word = config_filter(word)
+        if len(new_word) <= 0:
+            return ''
+        new_word_list.append(new_word)
+        if word in interpunction and config_capitalization[active_config-1]:
             follow_with_capital = True
     # Capitalize first word
-    new_word_list[0] = new_word_list[0].capitalize()
+    if config_capitalization[active_config-1]:
+        new_word_list[0] = new_word_list[0].capitalize()
     return ' '.join(new_word_list)
+
+# Changes a word for different configurations; returns '' when word is not allowed
+def config_filter(word):
+    new_word = ''
+    for c in word:
+        if c in config_ignore[active_config-1]:
+            continue
+        elif c in config_convert[active_config-1]:
+            new_word += config_convert[active_config-1].get(c)
+        elif c not in config_allowed[active_config-1]:
+            return ''
+        else:
+            new_word += c
+    return new_word
 
 # Function that tries to parse string to a number, if that goes turn it into a word
 def number_to_words(word):
