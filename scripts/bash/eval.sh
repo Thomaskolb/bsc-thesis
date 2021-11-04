be6dd81b56c9ea061fb1c1e02b3e0f303fe0ab86
Thomas <thomaskolb98@gmail.com>
2021-11-04 14:54:45 +0100

    stuff

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -19,8 +19,8 @@ python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
     --gen-subset test \
     --results-path ~/bsc/data/fairseq-evals/$datetime \
-    # --w2l-decoder kenlm \
-    # --lm-model /path/to/kenlm.bin \
+    --w2l-decoder kenlm \
+    --lm-model /path/to/kenlm.bin \
     --lm-weight 2 \
     --word-score -1 \
     --sil-weight 0 \
