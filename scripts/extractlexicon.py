2e553f22e9ae089c33c349286eee903249874ade
Thomas <thomaskolb98@gmail.com>
2021-11-08 17:19:00 +0100

    Lexicon code

---
+++ scripts/extractlexicon.py
@@ -0,0 +1,23 @@
+# Thomas Kolb s1027332
+# This program takes a file and extracts all the unique words
+
+import sys
+
+# Function that extracts the unique words in a list
+def extract_words(path):
+    word_list = []
+    with open(path, 'r') as ltrfile:
+        for word in [item for sublist in [w.split('|') for w in ltrfile.read().split('\n')] for item in sublist]:
+            if word not in word_list:
+                word_list.append(word)
+    return dict(sorted(word_list))
+
+# Function that writes data to output file
+def write_lexicon_file(word_list, outpath):
+    with open(outpath, 'w') as outfile:
+        [outfile.write(f'{" ".join(list(word))}\n') for word in word_list]
+    
+if len(sys.argv) < 3:
+    print("Please enter the path of the ltr file and the output file")
+else:
+    write_lexicon_file(extract_words(sys.argv[1]), sys.argv[2])
