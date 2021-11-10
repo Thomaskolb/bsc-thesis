79fb8614472d0b3b5480379923b890458c2976c3
Thomas <thomaskolb98@gmail.com>
2021-11-10 15:35:16 +0100

    fallback

--- scripts/bash/lmplease.sh
+++ scripts/bash/lmplease.sh
@@ -9,5 +9,5 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-~/bsc/data/kenlm/build/bin/lmplz -o 5 < ~/bsc/data/models/test.txt > ~/bsc/data/models/lmfile.arpa
+~/bsc/data/kenlm/build/bin/lmplz --discount_fallback -o 5 < ~/bsc/data/models/test.txt > ~/bsc/data/models/lmfile.arpa
 ~/bsc/data/kenlm/build/bin/build_binary ~/bsc/data/models/lmfile.arpa ~/bsc/data/models/lmfile.bin
