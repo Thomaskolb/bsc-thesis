7857d4450db3be4578e503b5984a3e98f684962c
Thomas <thomaskolb98@gmail.com>
2021-11-13 12:12:57 +0100

    date

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -9,11 +9,11 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-datetime="2021-11-09/02-12-02"
+datetime="2021-11-11/18-12-54"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
-    ~/bsc/data/tempdata3 \
+    ~/bsc/data/tempdata \
     --task audio_finetuning \
     --nbest 1 \
     --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
