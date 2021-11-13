61b8dd93b11093b9367b19ee7f71b781e907ed18
Thomas <thomaskolb98@gmail.com>
2021-11-13 16:55:19 +0100

    stuff

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -9,26 +9,26 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-datetime="2021-11-11/18-12-54"
+datetime="2021-11-12/10-12-27"
 valid_data_path="/home/tkolb/bsc/data/tempdata"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-# python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
-#     $valid_data_path \
-#     --task audio_finetuning \
-#     --nbest 1 \
-#     --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
-#     --gen-subset valid \
-#     --results-path ~/bsc/data/fairseq-evals/$datetime \
-#     --w2l-decoder kenlm \
-#     --lm-model ~/bsc/data/models/lmfile.bin \
-#     --lm-weight 2 \
-#     --lexicon ~/bsc/data/models/lexicon.txt \
-#     --word-score -1 \
-#     --sil-weight 0 \
-#     --criterion ctc \
-#     --labels ltr \
-#     --max-tokens 1000000 \
-#     --post-process letter
+python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
+    --gen-subset valid \
+    --results-path ~/bsc/data/fairseq-evals/$datetime \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/lmfile.bin \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/lexicon.txt \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
 python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime" $valid_data_path
 deactivate
