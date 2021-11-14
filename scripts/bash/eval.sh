170ff4ba580af8cf68008af75a4c7db692469fdd
Thomas <thomaskolb98@gmail.com>
2021-11-14 14:06:07 +0100

    test set

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 datetime="2021-11-11/18-12-54"
-valid_data_path="/home/tkolb/bsc/data/tempdata"
+valid_data_path="/home/tkolb/bsc/data/tempdata2"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
