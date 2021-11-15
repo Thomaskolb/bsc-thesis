dbd32ae157af2161da2068c0a51249d8cc00bfa3
Thomas <thomaskolb98@gmail.com>
2021-11-15 12:15:05 +0100

    lm 3

--- scripts/bash/lmplz.sh
+++ scripts/bash/lmplz.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-lmfile="c2lmfile"
+lmfile="c3lmfile"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ../LMgenerator.py "/home/tkolb/bsc/thomas/data" "/home/tkolb/bsc/data/models/$lmfile.txt"
