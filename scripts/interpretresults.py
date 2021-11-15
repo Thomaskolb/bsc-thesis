7ac40113b3d7322a60f39ee5131368840371e859
Thomas <thomaskolb98@gmail.com>
2021-11-15 14:06:05 +0100

    stuff

--- scripts/interpretresults.py
+++ scripts/interpretresults.py
@@ -23,8 +23,8 @@ def interpret_data(config_path_list):
     for config_path in config_path_list:
         with open(config_path, 'r') as file:
             print(config_path)
-            print(file.read().split('\n')[-2])
-            value = float(file.read().split('\n')[-2].split(' ')[-1])
+            print((file.read().split('\n')[-3]).split(' '))
+            value = float((file.read().split('\n')[-3]).split(' ')[-1])
             if not data_dict[f'configuration {config_id}']:
                 data_dict[f'configuration {config_id}'] = [value]
             else:
