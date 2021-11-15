e132337d53e8321f4516eed34051b04d1cc92216
Thomas <thomaskolb98@gmail.com>
2021-11-15 14:01:06 +0100

    vergeten

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -15,22 +15,22 @@ lmfile="lmfile.txt"
 lexicon="lexicon.txt"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
-    $valid_data_path \
-    --task audio_finetuning \
-    --nbest 1 \
-    --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
-    --gen-subset test \
-    --results-path ~/bsc/data/fairseq-evals/$datetime \
-    --w2l-decoder kenlm \
-    --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight 2 \
-    --lexicon ~/bsc/data/models/$lexicon \
-    --word-score -1 \
-    --sil-weight 0 \
-    --criterion ctc \
-    --labels ltr \
-    --max-tokens 1000000 \
-    --post-process letter
+# python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+#     $valid_data_path \
+#     --task audio_finetuning \
+#     --nbest 1 \
+#     --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
+#     --gen-subset test \
+#     --results-path ~/bsc/data/fairseq-evals/$datetime \
+#     --w2l-decoder kenlm \
+#     --lm-model ~/bsc/data/models/$lmfile \
+#     --lm-weight 2 \
+#     --lexicon ~/bsc/data/models/$lexicon \
+#     --word-score -1 \
+#     --sil-weight 0 \
+#     --criterion ctc \
+#     --labels ltr \
+#     --max-tokens 1000000 \
+#     --post-process letter
 python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime" $valid_data_path
 deactivate
