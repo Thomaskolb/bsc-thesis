620951ed15abf7ea6c23abd89a2024f700ec554f
Thomas <thomaskolb98@gmail.com>
2021-11-16 12:07:27 +0100

    asr stuff also

--- scripts/interpretresults.py
+++ scripts/interpretresults.py
@@ -16,12 +16,6 @@ def write_data(data, outpath):
                 print(i)
                 print(data[config])
                 tf.summary.scalar('results', data[config][i], step=i)
-    # ASR data
-    file_writer = tf.summary.create_file_writer(f'{outpath}/asr-results')
-    with file_writer.as_default():
-        tf.summary.scalar('results', 0.31011388388465866, step=0)
-        tf.summary.scalar('results', 0.31011388388465866, step=1)
-        tf.summary.scalar('results', 0.31011388388465866, step=2)
 
 # Interpret data given from differen configuration outputs
 def interpret_data(config_path_list):
@@ -33,10 +27,14 @@ def interpret_data(config_path_list):
             print(config_path)
             number = (file.read().split('\n')[-3]).split(' ')[-1]
             value = float(number)
+            asr_number = (file.read().split('\n')[-2]).split(' ')[-1]
+            asr_value = float(asr_number)
             if f'configuration {config_id}' not in data_dict:
                 data_dict[f'configuration {config_id}'] = [value]
+                data_dict[f'asr {config_id}'] = [asr_value]
             else:
                 data_dict[f'configuration {config_id}'].append(value)
+                data_dict[f'asr {config_id}'].append(asr_value)
         dataset_id = (dataset_id + 1) % dps_per_config
         if dataset_id == 0:
             config_id += 1
