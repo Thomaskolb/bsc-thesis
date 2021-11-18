76edbdacb63bec0cce1a7ccd894adaed03be9049
Thomas <thomaskolb98@gmail.com>
2021-11-18 14:17:07 +0100

    lelijke forlooploze code

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -9,31 +9,66 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-DATETIMES=("2021-11-11/18-12-54" "2021-11-12/10-12-27" "2021-11-12/10-34-58")
+datetime1="2021-11-11/18-12-54"
+datetime2="2021-11-12/10-12-27"
+datetime3="2021-11-12/10-34-58"
 valid_data_path="/home/tkolb/bsc/data/c2tempdata2"
-lmfile="c1lmfile.bin"
-lexicon="c1lexicon.txt"
+lmfile="c2lmfile.bin"
+lexicon="c2lexicon.txt"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-for datetime in $DATETIMES; do
-    echo $datetime
-    python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
-        $valid_data_path \
-        --task audio_finetuning \
-        --nbest 1 \
-        --path ~/bsc/data/fairseq-outputs/$datetime/checkpoints/checkpoint_best.pt \
-        --gen-subset test \
-        --results-path ~/bsc/data/fairseq-evals/$datetime \
-        --w2l-decoder kenlm \
-        --lm-model ~/bsc/data/models/$lmfile \
-        --lm-weight 2 \
-        --lexicon ~/bsc/data/models/$lexicon \
-        --word-score -1 \
-        --sil-weight 0 \
-        --criterion ctc \
-        --labels ltr \
-        --max-tokens 1000000 \
-        --post-process letter
-    python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime" $valid_data_path
-done
+python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/fairseq-outputs/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset test \
+    --results-path ~/bsc/data/fairseq-evals/$datetime1 \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime1" $valid_data_path
+python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/fairseq-outputs/$datetime2/checkpoints/checkpoint_best.pt \
+    --gen-subset test \
+    --results-path ~/bsc/data/fairseq-evals/$datetime2 \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime2" $valid_data_path
+python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/fairseq-outputs/$datetime3/checkpoints/checkpoint_best.pt \
+    --gen-subset test \
+    --results-path ~/bsc/data/fairseq-evals/$datetime3 \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime3" $valid_data_path
 deactivate
