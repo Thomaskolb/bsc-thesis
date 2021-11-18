41dda4c7dac75af6c2793442e35f3f26dceda5f6
Thomas <thomaskolb98@gmail.com>
2021-11-18 16:20:50 +0100

    xlrs

---
+++ scripts/bash/train-xlrs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#SBATCH --partition=csedu
+#SBATCH --gres=gpu:4
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=4
+#SBATCH --time=48:00:00
+#SBATCH --output=train-%J.out
+#SBATCH --error=train-%J.err
+#SBATCH --mail-user=thomaskolb@live.nl
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
+fairseq-hydra-train \
+    task.data=/home/tkolb/bsc/data/c2tempdata \
+    model.w2v_path=/home/tkolb/bsc/data/models/xlsr_53_56k.pt \
+    model.freeze_finetune_updates=0 \
+    --config-dir /home/tkolb/bsc/bsc-thesis/scripts/fairseq \
+    --config-name xlrs
+deactivate
