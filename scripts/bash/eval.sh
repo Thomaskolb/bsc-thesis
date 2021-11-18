34d01d86cd9212c9bb6efec341f731e0084b189a
Thomas <thomaskolb98@gmail.com>
2021-11-18 14:20:02 +0100

    ditdanmaar

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -13,8 +13,8 @@ datetime1="2021-11-11/18-12-54"
 datetime2="2021-11-12/10-12-27"
 datetime3="2021-11-12/10-34-58"
 valid_data_path="/home/tkolb/bsc/data/c2tempdata2"
-lmfile="c2lmfile.bin"
-lexicon="c2lexicon.txt"
+lmfile="c1lmfile.bin"
+lexicon="c1lexicon.txt"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
