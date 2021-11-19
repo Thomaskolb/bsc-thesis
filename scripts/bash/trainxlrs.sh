a3c48452213abad9c72ac4f13ea3bab0717bb144
Thomas <thomaskolb98@gmail.com>
2021-11-19 14:39:11 +0100

    75g

--- scripts/bash/trainxlrs.sh
+++ scripts/bash/trainxlrs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #SBATCH --partition=csedu
 #SBATCH --gres=gpu:2
-#SBATCH --mem=100G
+#SBATCH --mem=75G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
 #SBATCH --output=trainxlrs-%J.out
