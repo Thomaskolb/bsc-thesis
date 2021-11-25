43d6131ff93ab750f3a1063b8edc0d39d52b21a2
Thomas <thomaskolb98@gmail.com>
2021-11-25 15:32:54 +0100

    16 l'

--- scripts/bash/analyze.sh
+++ scripts/bash/analyze.sh
@@ -13,6 +13,6 @@ basepath="/home/tkolb/bsc/data/fairseq-evals"
 gen_subset="collect"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-python3 ../analyzecaptions.py "/home/tkolb/bsc/data/analysis/$gen_subset/eh32_low.txt" \
-    "$basepath/2021-11-14/15-30-40/$gen_subset"
+python3 ../analyzecaptions.py "/home/tkolb/bsc/data/analysis/$gen_subset/eh16_low.txt" \
+    "$basepath/2021-11-14/15-23-43/$gen_subset"
 deactivate
