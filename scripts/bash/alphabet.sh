a2ca700415eaf68853d0b95da2c2280cfaa28119
Thomas <thomaskolb98@gmail.com>
2021-12-03 23:27:02 +0100

    stuff

--- scripts/bash/alphabet.sh
+++ scripts/bash/alphabet.sh
@@ -10,5 +10,5 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-python3 ../extractalphabet.py "/home/tkolb/bsc/data/c3tempdata3/train.ltr" "/home/tkolb/bsc/data/c3tempdata3/dict.ltr.txt"
+python3 ../extractalphabet.py "/home/tkolb/bsc/data/ctc2tempdata/train.ltr" "/home/tkolb/bsc/data/ctc2tempdata/dict.ltr.txt"
 deactivate
