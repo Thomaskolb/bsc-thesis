21a4873b0d8661ed77346df7975aa7d4348383a9
Thomas <thomaskolb98@gmail.com>
2021-12-04 16:40:37 +0100

    2again

--- scripts/bash/train.sh
+++ scripts/bash/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #SBATCH --partition=csedu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=50G
+#SBATCH --mem=80G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
 #SBATCH --output=train-%J.out
@@ -11,7 +11,7 @@
 
 source ~/.cache/pypoetry/virtualenvs/kolb-poetry-SgAmqU3s-py3.6/bin/activate
 fairseq-hydra-train \
-    task.data=/home/tkolb/bsc/data/ctc2tempdata3 \
+    task.data=/home/tkolb/bsc/data/ctc2tempdata2 \
     model.w2v_path=/home/tkolb/bsc/data/models/wav2vec_small.pt \
     model.freeze_finetune_updates=10000 \
     --config-dir /home/tkolb/bsc/bsc-thesis/scripts/fairseq \
