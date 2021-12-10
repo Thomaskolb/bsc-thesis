b296df464eacb0cd7e0138eb0426bc6454032f6d
Thomas <thomaskolb98@gmail.com>
2021-12-10 22:33:34 +0100

    stuff'

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-#SBATCH --partition=csedu
+#SBATCH --partition=das
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50G
 #SBATCH --cpus-per-task=1
@@ -9,22 +9,24 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-datetime1="2021-11-14/15-14-42"
-datetime2="2021-11-14/15-23-43"
-datetime3="2021-11-14/15-30-40"
-valid_data_path="/home/tkolb/bsc/data/c2tempdata3"
+datetime1="2021-12-03/14-49-23"
+datetime2="2021-12-08/19-47-17"
+datetime3="2021-12-06/15-48-32"
+valid_data_path="/home/tkolb/bsc/data/ctc2tempdata3"
 lmfile="c2lmfile.bin"
 lexicon="c2lexicon.txt"
 gen_subset="test"
+outputsfolder="fairseq-xlsr-outputs"
+evalsfolder="fairseq-xlsr-evals"
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     $valid_data_path \
     --task audio_finetuning \
     --nbest 1 \
-    --path ~/bsc/data/fairseq-outputs/$datetime1/checkpoints/checkpoint_best.pt \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
     --gen-subset $gen_subset \
-    --results-path ~/bsc/data/fairseq-evals/$datetime1/$gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
     --lm-weight 2 \
@@ -35,14 +37,14 @@ python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --labels ltr \
     --max-tokens 1000000 \
     --post-process letter
-python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime1/$gen_subset" $valid_data_path
+python3 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset" $valid_data_path
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     $valid_data_path \
     --task audio_finetuning \
     --nbest 1 \
-    --path ~/bsc/data/fairseq-outputs/$datetime2/checkpoints/checkpoint_best.pt \
+    --path ~/bsc/data/$outputsfolder/$datetime2/checkpoints/checkpoint_best.pt \
     --gen-subset $gen_subset \
-    --results-path ~/bsc/data/fairseq-evals/$datetime2/$gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime2/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
     --lm-weight 2 \
@@ -53,14 +55,14 @@ python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --labels ltr \
     --max-tokens 1000000 \
     --post-process letter
-python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime2/$gen_subset" $valid_data_path
+python3 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime2/$gen_subset" $valid_data_path
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     $valid_data_path \
     --task audio_finetuning \
     --nbest 1 \
-    --path ~/bsc/data/fairseq-outputs/$datetime3/checkpoints/checkpoint_best.pt \
+    --path ~/bsc/data/$outputsfolder/$datetime3/checkpoints/checkpoint_best.pt \
     --gen-subset $gen_subset \
-    --results-path ~/bsc/data/fairseq-evals/$datetime3/$gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime3/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
     --lm-weight 2 \
@@ -71,5 +73,5 @@ python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --labels ltr \
     --max-tokens 1000000 \
     --post-process letter
-python3 ../extractWER.py "/home/tkolb/bsc/data/fairseq-evals/$datetime3/$gen_subset" $valid_data_path
+python3 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime3/$gen_subset" $valid_data_path
 deactivate
