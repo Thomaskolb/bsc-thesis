75d2f812cf04dd85647a844e5875639a0afaaef8
Thomas <thomaskolb98@gmail.com>
2021-12-10 23:22:12 +0100

    aah

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -19,7 +19,7 @@ gen_subset="test"
 outputsfolder="fairseq-outputs-xlsr"
 evalsfolder="fairseq-evals-xlsr"
 
-source ~/.cache/pypoetry/virtualenvs/thomas-poetry-yCU5QAa0-py3.8/bin/activate
+source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
 python3 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     $valid_data_path \
     --task audio_finetuning \
