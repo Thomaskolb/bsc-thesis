d28ee4375fe27ffc492327d8e4ac6f763fb969a8
Thomas <thomaskolb98@gmail.com>
2021-12-24 11:16:10 +0100

    var

--- scripts/bash/eval_var.sh
+++ scripts/bash/eval_var.sh
@@ -30,7 +30,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight 2 \
+    --lm-weight 0 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score -1 \
     --sil-weight 0 \
@@ -38,5 +38,149 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --labels ltr \
     --max-tokens 1000000 \
     --post-process letter
-python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "base"
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "lm0"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 1 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "lm1"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 1 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "lm1"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 3 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "lm3"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 4 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "lm4"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score -2 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "ws-2"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score 0 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "ws0"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score 1 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "ws1"
+python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
+    $valid_data_path \
+    --task audio_finetuning \
+    --nbest 1 \
+    --path ~/bsc/data/$outputsfolder/$datetime1/checkpoints/checkpoint_best.pt \
+    --gen-subset $gen_subset \
+    --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset/var \
+    --w2l-decoder kenlm \
+    --lm-model ~/bsc/data/models/$lmfile \
+    --lm-weight 2 \
+    --lexicon ~/bsc/data/models/$lexicon \
+    --word-score 2 \
+    --sil-weight 0 \
+    --criterion ctc \
+    --labels ltr \
+    --max-tokens 1000000 \
+    --post-process letter
+python3.8 ../extractWER.py "/home/tkolb/bsc/data/$evalsfolder/$datetime1/$gen_subset/var" $valid_data_path "ws2"
 deactivate
