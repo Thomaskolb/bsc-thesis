32c949976ba2e43beb45996958f13d913318c7af
Thomas <thomaskolb98@gmail.com>
2021-12-24 17:43:34 +0100

    1

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -32,7 +32,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight 2 \
+    --lm-weight 0 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score 0 \
     --sil-weight 0 \
@@ -50,7 +50,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime2/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight 1 \
+    --lm-weight 0 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score 0 \
     --sil-weight 0 \
@@ -68,7 +68,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime3/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight 1 \
+    --lm-weight 0 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score 0 \
     --sil-weight 0 \
