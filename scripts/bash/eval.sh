05cb02e26dca8ac165bb87cba5d24399383bc621
Thomas <thomaskolb98@gmail.com>
2021-12-28 17:07:56 +0100

    1

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -42,8 +42,8 @@ valid_data_path="/home/tkolb/bsc/data/c2tempdata"
 lmfile="c2lmfile.bin"
 lexicon="c2lexicon.txt"
 gen_subset="test"
-outputsfolder="fairseq-outputs"
-evalsfolder="fairseq-evals-base"
+outputsfolder="fairseq-outputs-xlsr"
+evalsfolder="fairseq-evals-xlsr"
 
 source ~/.cache/pypoetry/virtualenvs/new-env-xry5bPeK-py3.8/bin/activate
 python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
