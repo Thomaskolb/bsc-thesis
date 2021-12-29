0bf748b98aba0deb6eb5e567e425955216b20023
Thomas <thomaskolb98@gmail.com>
2021-12-29 12:28:49 +0100

    2

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -26,26 +26,26 @@
 # datetime2="2021-12-08/15-10-32"
 # datetime3="2021-12-04/16-34-35"
 # c
-# datetime1="2021-11-14/15-14-42"
-# datetime2="2021-11-14/15-23-43"
-# datetime3="2021-11-14/15-30-40"
+datetime1="2021-11-14/15-14-42"
+datetime2="2021-11-14/15-23-43"
+datetime3="2021-11-14/15-30-40"
 # xlsr
 # ctc
 # datetime1="2021-12-16/12-12-56"
 # datetime2="2021-12-16/12-15-36"
 # datetime3="2021-12-12/11-41-57"
 # c
-datetime1="2021-12-20/16-11-51"
-datetime2="2021-12-20/16-23-34"
-datetime3="2021-12-20/16-24-42"
+# datetime1="2021-12-20/16-11-51"
+# datetime2="2021-12-20/16-23-34"
+# datetime3="2021-12-20/16-24-42"
 
 valid_data_path="/home/tkolb/bsc/data/c2tempdata"
 lmfile="c2lmfile.bin"
 lexicon="c2lexicon.txt"
 gen_subset="test"
-outputsfolder="fairseq-outputs-xlsr"
-evalsfolder="fairseq-evals-xlsr/opt"
-lm=2
+outputsfolder="fairseq-outputs"
+evalsfolder="fairseq-evals-base/opt"
+lm=1
 ws=0
 
 source ~/.cache/pypoetry/virtualenvs/new-env-xry5bPeK-py3.8/bin/activate
