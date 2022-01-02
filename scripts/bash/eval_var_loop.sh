d6583c2b1a67c883deca25b81c96d80c32913ba7
Thomas <thomaskolb98@gmail.com>
2022-01-02 18:31:51 +0100

    xq

--- scripts/bash/eval_var_loop.sh
+++ scripts/bash/eval_var_loop.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-datetime1="2021-12-20/16-23-34"
+datetime1="2021-12-20/16-11-51"
 valid_data_path="/home/tkolb/bsc/data/c2tempdata"
 lmfile="c2lmfile.bin"
 lexicon="c2lexicon.txt"
