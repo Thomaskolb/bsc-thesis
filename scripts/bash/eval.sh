7752502979d63a2b63e31df2719c57f3ba7d15dc
Thomas <thomaskolb98@gmail.com>
2022-01-03 13:31:04 +0100

    iiiiiiiihiiihii

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -26,9 +26,9 @@
 # datetime2="2021-12-08/15-10-32"
 # datetime3="2021-12-04/16-34-35"
 # c
-# datetime1="2021-11-14/15-14-42"
-# datetime2="2021-11-14/15-23-43"
-# datetime3="2021-11-14/15-30-40"
+datetime1="2021-11-14/15-14-42"
+datetime2="2021-11-14/15-23-43"
+datetime3="2021-11-14/15-30-40"
 # xlsr
 # ctc
 # datetime1="2021-12-16/12-12-56"
@@ -39,13 +39,12 @@
 # datetime2="2021-12-20/16-23-34"
 # datetime3="2021-12-20/16-24-42"
 
-valid_data_path="/home/tkolb/bsc/data/ctc2tempdata"
+valid_data_path="/home/tkolb/bsc/data/c2tempdata"
 lmfile="c2lmfile.bin"
 lexicon="c2lexicon.txt"
 gen_subset="test"
 outputsfolder="fairseq-outputs"
 evalsfolder="fairseq-evals-base/opt"
-lm=1
 ws=0
 
 source ~/.cache/pypoetry/virtualenvs/new-env-xry5bPeK-py3.8/bin/activate
@@ -58,7 +57,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime1/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight $lm \
+    --lm-weight 2.0 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score $ws \
     --sil-weight 0 \
@@ -76,7 +75,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime2/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight $lm \
+    --lm-weight 1.2 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score $ws \
     --sil-weight 0 \
@@ -94,7 +93,7 @@ python3.8 ~/bsc/fairseq/examples/speech_recognition/infer.py \
     --results-path ~/bsc/data/$evalsfolder/$datetime3/$gen_subset \
     --w2l-decoder kenlm \
     --lm-model ~/bsc/data/models/$lmfile \
-    --lm-weight $lm \
+    --lm-weight 1.2 \
     --lexicon ~/bsc/data/models/$lexicon \
     --word-score $ws \
     --sil-weight 0 \
