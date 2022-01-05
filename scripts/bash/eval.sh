0d3220b4bc4065d94ec2c3425704366a84dec77f
Thomas <thomaskolb98@gmail.com>
2022-01-05 13:44:56 +0100

    2

--- scripts/bash/eval.sh
+++ scripts/bash/eval.sh
@@ -9,13 +9,13 @@
 #SBATCH --mail-user=thomaskolb@live.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
 # config 1
-datetime1="2021-11-11/18-12-54"
-datetime2="2021-11-12/10-12-27"
-datetime3="2021-11-12/10-34-58"
+# datetime1="2021-11-11/18-12-54"
+# datetime2="2021-11-12/10-12-27"
+# datetime3="2021-11-12/10-34-58"
 # config 2
-# datetime1="2021-11-14/15-14-42"
-# datetime2="2021-11-14/15-23-43"
-# datetime3="2021-11-14/15-30-40"
+datetime1="2021-11-14/15-14-42"
+datetime2="2021-11-14/15-23-43"
+datetime3="2021-11-14/15-30-40"
 # config 3
 # datetime1="2021-11-15/11-44-06"
 # datetime2="2021-11-15/11-45-07"
@@ -42,9 +42,9 @@ datetime3="2021-11-12/10-34-58"
 # configs = 11-12|11-11|11-14|11-15 ctc = 12-04|12-08
 # xlsr = 12-20 ctc = 2021-12-16/12-12-56|2021-12-16/12-15-36|2021-12-12/11-41-57
 
-valid_data_path="/home/tkolb/bsc/data/testset/c1h8"
-lmfile="c1lmfile.bin"
-lexicon="c1lexicon.txt"
+valid_data_path="/home/tkolb/bsc/data/testset/c2h8"
+lmfile="c2lmfile.bin"
+lexicon="c2lexicon.txt"
 gen_subset="test"
 outputsfolder="fairseq-outputs"
 evalsfolder="fairseq-evals-base/newtest"
