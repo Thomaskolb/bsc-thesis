e86a51f332c986389604f5185cbab83c655fd292
Thomas <thomaskolb98@gmail.com>
2022-01-05 14:15:24 +0100

    ctc

--- scripts/samplegenerator.py
+++ scripts/samplegenerator.py
@@ -39,7 +39,7 @@ add_stepsize = 0.1
 add_range = 30
 
 # Allow strict caption time subtraction
-subtract_caption_time = False
+subtract_caption_time = True
 
 # Function that creates the same folders as found in the datapath directory
 def create_directories(datapath, outputpath):
