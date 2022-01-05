d56c3d1910bb3d92a6495fbf4a8bae8a8513f71e
Thomas <thomaskolb98@gmail.com>
2022-01-05 11:12:14 +0100

    dict

--- scripts/bash/alphabet.sh
+++ scripts/bash/alphabet.sh
@@ -10,5 +10,5 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source ~/.cache/pypoetry/virtualenvs/tkolbpoetry-0grRN4_Q-py3.6/bin/activate
-python3 ../extractalphabet.py "/home/tkolb/bsc/data/testdata/c1h8/train.ltr" "/home/tkolb/bsc/data/testdata/c1h8/dict.ltr.txt"
+python3 ../extractalphabet.py "/home/tkolb/bsc/data/testdata/c1h8/test.ltr" "/home/tkolb/bsc/data/testdata/c1h8/dict.ltr.txt"
 deactivate
